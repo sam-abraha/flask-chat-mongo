f2582c69dcd667d07abf11a6ebad539e6cc1e0fd
sam <samuelabraha@gmail.com>
2026-06-01 17:16:09 +0200

    feat: add room repositories functions inside room service

--- room_service.py
+++ room_service.py
@@ -1,19 +1,23 @@
 import random
 from string import ascii_uppercase
-from db import save_room, get_room, update_room, delete_room
-
+from repositories.room_repository import (
+    insert_room,
+    find_room_by_code,
+    update_room_by_code,
+    delete_room_by_code
+)
 
 def create_room_code(length=5):
     while True:
         code = "".join(random.choices(ascii_uppercase, k=length))
 
-        if not get_room(code):
+        if not find_room_by_code(code):
             return code
 
 
 def create_new_room(owner_id):
     room_code = create_room_code()
-    save_room(room_code, owner_id)
+    insert_room(room_code, owner_id)
     return room_code
 
 
@@ -21,7 +25,7 @@ def can_join_room(room_code):
     if not room_code:
         return False, "Please enter a room code"
 
-    room = get_room(room_code)
+    room = find_room_by_code(room_code)
 
     if not room:
         return False, "Room does not exist"
@@ -29,25 +33,25 @@ def can_join_room(room_code):
     return True, None
 
 def increment_room_members(room_code):
-    room = get_room(room_code)
+    room = find_room_by_code(room_code)
 
     if not room:
         return False
 
     members = room.get("members", 0) + 1
-    update_room(room_code, {"members": members})
+    update_room_by_code(room_code, {"members": members})
 
     return True
 
 
 def decrement_room_members(room_code):
-    room = get_room(room_code)
+    room = find_room_by_code(room_code)
 
     if not room:
         return False
 
     members = max(room.get("members", 1) - 1, 0)
-    update_room(room_code, {"members": members})
+    update_room_by_code(room_code, {"members": members})
 
     return True
 
@@ -55,7 +59,7 @@ def validate_room_session(room_code, name):
     if not room_code or not name:
         return False, None
 
-    room = get_room(room_code)
+    room = find_room_by_code(room_code)
 
     if not room:
         return False, None
@@ -63,7 +67,7 @@ def validate_room_session(room_code, name):
     return True, room
 
 def delete_room_if_owner(room_code, user_id):
-    room = get_room(room_code)
+    room = find_room_by_code(room_code)
 
     if not room:
         return False,"Room not found"
@@ -71,5 +75,5 @@ def delete_room_if_owner(room_code, user_id):
     if room.get("owner_id") != str(user_id):
         return False,"You are not allowed to delete this room"
     
-    delete_room(room_code)
+    delete_room_by_code(room_code)
     return True, None
