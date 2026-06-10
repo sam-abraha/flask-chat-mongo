0954b53f5680a70587a52af496e0c49ba712558e
sam <samuelabrh@gmail.com>
2026-06-10 12:58:07 +0200

    chore: use gevent gunicorn worker for railway deployment

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,3 @@
-from app import app, socketio
-import eventlet
-import eventlet.wsgi
+from app import app
 
-if __name__ == "__main__":
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8080)), app)
+application = app
